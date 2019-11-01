9e133a9671ac6f9de8dc9370b8bb574adbdf4fba
Tejas Patkar <tspatkar@gmail.com>
2019-11-01 14:39:42 +0530

    register controller updated

--- faculty/register/register.controller.js
+++ faculty/register/register.controller.js
@@ -28,10 +28,10 @@ function Controller($location, $rootScope, RegistrationService){
 
 
 
-    //window.location.href = "#!/dashboard";
+    
 
   }
 
-
+//window.location.href = "#!/dashboard";
 
 }
