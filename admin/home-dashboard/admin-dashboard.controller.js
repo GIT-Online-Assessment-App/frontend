d01a88316c6775a1358efd1b4af5da56c80ca5c3
Tejas Patkar <tspatkar@gmail.com>
2019-12-25 21:36:51 +0530

    navbar to Admin dashboard

--- admin/home-dashboard/admin-dashboard.controller.js
+++ admin/home-dashboard/admin-dashboard.controller.js
@@ -1,7 +1,15 @@
 app.controller("AdminDashboard.HomeController", Controller);
 
 function Controller($localStorage,DashboardService,$routeParams, AuthenticationService){
+    var admDshHm = this;
 
-    
+    initController();
+    function initController(){
+        if($localStorage.currentUser){
+            admDshHm.uname = $localStorage.currentUser.username;
+        }
+    }
+
+
 
 }
