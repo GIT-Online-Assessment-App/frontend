f9dc331d28b7c19aa7816c213dae40790ec0ad4e
Tejas Patkar <tspatkar@gmail.com>
2019-12-30 09:46:18 +0530

    minor bug fixes

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function run($rootScope, $http, $location, $localStorage) {
         $rootScope.URLs = {
         //"ip" : "http://192.168.43.206"
         //"ip"  : "http://10.10.2.219"
-        "ip" : "http://10.10.3.160"
+        "ip" : "http://10.10.5.47"
         }
 
 
